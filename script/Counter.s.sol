076f88f802e4c18e07aa093c5a63f51047912758
Jason Yapri <jason.yapri@gmail.com>
2024-03-08 12:20:44 +0700

    Manual Deployment Script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -10,3 +10,10 @@ contract CounterScript is Script {
         vm.broadcast();
     }
 }
+
+// Deploy the smart contract
+// forge create --rpc-url https://sepolia.infura.io/v3/[api key] --private-key [private key] src/Counter.sol:Counter
+// Get Public State Variable
+// cast call [smart contract address] "number()(uint256)" --rpc-url https://sepolia.infura.io/v3/[api key]
+// Modify State Variable through External / Public Function
+// cast send [smart contract address] "increment()" --rpc-url https://sepolia.infura.io/v3/[api key] --private-key [private key]
